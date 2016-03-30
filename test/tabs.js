d881d84b6ec73431e9995da41c89e29935943b3d
Sergey Chikuyonok <serge.che@gmail.com>
2016-03-31 01:35:58 +0300

    Initial implementation of tbas reducer

---
+++ test/tabs.js
@@ -0,0 +1,14 @@
+/**
+ * Test `tabs` key reducer
+ */
+'use strict';
+
+const assert = require('assert');
+const redux = require('redux');
+require('babel-register');
+const reducer = require('../lib/reducers/tabs').default;
+const TAB = require('../lib/action-names').TAB;
+
+describe('Tabs reducer', () => {
+    
+});
