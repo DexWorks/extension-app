a41742c7121e0a15308d1ff6c4de76a7b4e60241
Sergey Chikuyonok <serge.che@gmail.com>
2016-04-05 01:42:22 +0300

    Test Remote View listener

--- test/remote-view.js
+++ test/remote-view.js
@@ -8,6 +8,7 @@ const redux = require('redux');
 const clientShim = require('./shim/client');
 require('babel-register');
 const reducer = require('../lib/reducers/remote-view').default;
+const listener = require('../lib/listeners/remote-view').default;
 const REMOTE_VIEW = require('../lib/action-names').REMOTE_VIEW;
 
 describe('Remote View', () => {
@@ -19,14 +20,17 @@ describe('Remote View', () => {
             sessions: new Map()
         });
         dispatch = action => store.dispatch(action);
-        // client = clientShim();
-        // unsubscribe = listener(client, store);
-        // emit = (name, data) => client.emit(name, data);
+        client = clientShim();
+        unsubscribe = listener(client, store);
+        emit = (name, data) => {
+            client.emit('message-receive', name, data);
+            client.emit(name, data);
+        };
     });
 
     afterEach(() => {
         client = store = dispatch = emit = null;
-        // unsubscribe();
+        unsubscribe();
     });
 
     describe('reducer', () => {
@@ -77,6 +81,114 @@ describe('Remote View', () => {
             assert.equal(sessions.size, 1);
             assert.equal(sessions.get('http://localhost:8081').state, REMOTE_VIEW.STATE_CONNECTED);
             assert.equal(sessions.get('http://localhost:8081').publicId, 'rv2.livestyle.io');
+        });
+    });
+
+    describe('listener', () => {
+        it('ping-pong', done => {
+            client.on('rv-ping', () => emit('rv-pong'));
+            emit('rv-connection');
+            setTimeout(() => {
+                assert.equal(store.getState().connected, true);
+                done();
+            }, 10);
+        });
+
+        it('session created', () => {
+            emit('rv-session', {
+                publicId: 'rv.livestyle.io',
+                localSite: 'http://localhost:8080',
+                connectUrl: 'http://livestyle.io:9001',
+                expiresAt: Date.now() + 3000
+            });
+
+            var sessions = store.getState().sessions;
+            assert.equal(sessions.size, 1);
+            assert.equal(sessions.get('http://localhost:8080').state, 'connected');
+            assert.equal(sessions.get('http://localhost:8080').publicId, 'rv.livestyle.io');
+
+            // session message with error: should not be added into sessions list
+            emit('rv-session', {
+                publicId: 'rv2.livestyle.io',
+                localSite: 'http://localhost:8081',
+                connectUrl: 'http://livestyle.io:9001',
+                error: {}
+            });
+
+            sessions = store.getState().sessions;
+            assert.equal(sessions.size, 1);
+            assert.equal(sessions.get('http://localhost:8080').publicId, 'rv.livestyle.io');
+        });
+
+        it('session closed', () => {
+            emit('rv-session', {
+                publicId: 'rv.livestyle.io',
+                localSite: 'http://localhost:8080',
+                connectUrl: 'http://livestyle.io:9001',
+                expiresAt: Date.now() + 3000
+            });
+
+            var sessions = store.getState().sessions;
+            assert.equal(sessions.size, 1);
+            assert.equal(sessions.get('http://localhost:8080').publicId, 'rv.livestyle.io');
+
+            emit('rv-session-closed', {localSite: 'http://localhost:8080'});
+            assert.equal(store.getState().sessions.size, 0);
+        });
+
+        it('update session list', () => {
+            emit('rv-session-list', [{
+                publicId: 'rv.livestyle.io',
+                localSite: 'http://localhost:8080',
+                connectUrl: 'http://livestyle.io:9001',
+                expiresAt: Date.now() + 3000
+            }, {
+                publicId: 'rv2.livestyle.io',
+                localSite: 'http://localhost:8081',
+                connectUrl: 'http://livestyle.io:9001',
+                expiresAt: Date.now() + 3000
+            }]);
+
+            var sessions = store.getState().sessions;
+            assert.equal(sessions.size, 2);
+            assert.equal(sessions.get('http://localhost:8080').publicId, 'rv.livestyle.io');
+            assert.equal(sessions.get('http://localhost:8081').publicId, 'rv2.livestyle.io');
+        });
+
+        it('request session list on client connect', done => {
+            client.on('rv-ping', () => emit('rv-pong'));
+            client.on('rv-get-session-list', () => emit('rv-session-list', [{
+                    publicId: 'rv.livestyle.io',
+                    localSite: 'http://localhost:8080',
+                    connectUrl: 'http://livestyle.io:9001',
+                    expiresAt: Date.now() + 3000
+                }, {
+                    publicId: 'rv2.livestyle.io',
+                    localSite: 'http://localhost:8081',
+                    connectUrl: 'http://livestyle.io:9001',
+                    expiresAt: Date.now() + 3000
+                }])
+            );
+
+            emit('client-connect');
+            setTimeout(() => {
+                var sessions = store.getState().sessions;
+                assert.equal(sessions.size, 2);
+                assert.equal(sessions.get('http://localhost:8080').publicId, 'rv.livestyle.io');
+                assert.equal(sessions.get('http://localhost:8081').publicId, 'rv2.livestyle.io');
+                done();
+            }, 10);
+        });
+
+        it('disconnect from app', done => {
+            client.on('rv-ping', () => emit('rv-pong'));
+            emit('rv-connection');
+            setTimeout(() => {
+                assert.equal(store.getState().connected, true);
+                emit('close');
+                assert.equal(store.getState().connected, false);
+                done();
+            }, 10);
         });
     });
 });
--- test/shim/client.js
+++ test/shim/client.js
@@ -6,6 +6,15 @@ const EventEmitter = require('events');
 
 module.exports = function() {
     var client = new EventEmitter();
+    var on = client.on;
+    client.on = function(name, callback) {
+        name.split(/\s+/).filter(Boolean)
+        .forEach(event => on.call(client, event, callback));
+        return this;
+    };
     client.off = client.removeListener;
+    client.send = function(name, data) {
+        client.emit(name, data);
+    };
     return client;
 };
